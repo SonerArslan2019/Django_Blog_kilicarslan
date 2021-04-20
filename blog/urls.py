bc6b2414d9b38eda974931bd8df523dd7ab4f4d8
cprm <soner@arslanyapi.com.tr>
2021-04-20 21:35:22 +0300

    class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from blog.views import iletisim, anasayfa, kategori, yazilarim, detay, yazi_ekle, yazi_guncelle, yazi_sil, yorum_sil
+from blog.views import iletisim, anasayfa, KategoriListView, yazilarim, DetayView, yazi_ekle, yazi_guncelle, yazi_sil, yorum_sil
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
     path('iletisim/', iletisim, name='iletisim'),
-    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
+    path('yonlendir/', RedirectView.as_view(url='https://www.getbootstrap.com/'), name='yonlendir'),
+    path('hakkimda/', TemplateView.as_view(template_name='pages/hakkimda.html'), name='hakkimda'),
+    path('kategori/<slug:kategoriSlug>', KategoriListView.as_view(), name='kategori'),
     path('yazilarim', yazilarim, name='yazilarim'),
-    path('detay/<slug:slug>', detay, name='detay'),
+    path('detay/<slug:slug>', DetayView.as_view(), name='detay'),
     path('yazi-sil/<slug:slug>', yazi_sil, name='yazi-sil'),
     path('yazi-ekle', yazi_ekle, name='yazi-ekle'),
     path('yorum-sil/<int:id>', yorum_sil, name='yorum-sil'),
--- blog/views/detay.py
+++ blog/views/detay.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import YazilarModel
 from blog.forms import YorumEkleModelForm
+from django.views.generic import View
+from django.contrib import messages
 
 
-def detay(request, slug):
-    yazi = get_object_or_404(YazilarModel, slug=slug)
-    yorumlar = yazi.yorumlar.all()
+class DetayView(View):
+    http_method_names = ['get', 'post']
+    yorum_ekleme_formu = YorumEkleModelForm
 
-    if request.method == 'POST':
-        yorum_ekle_form = YorumEkleModelForm(data=request.POST)
+    def get(self, request, slug):
+        yazi = get_object_or_404(YazilarModel, slug=slug)
+        yorumlar = yazi.yorumlar.all()
+
+        return render(request, 'pages/detay.html', context={
+            'yazi': yazi,
+            'yorumlar': yorumlar,
+            'yorum_ekle_form': self.yorum_ekleme_formu()
+        })
+
+    def post(self, request, slug):
+        yazi = get_object_or_404(YazilarModel, slug=slug)
+        yorum_ekle_form = self.yorum_ekleme_formu(data=request.POST)
         if yorum_ekle_form.is_valid():
             yorum = yorum_ekle_form.save(commit=False)
             yorum.yazan = request.user
             yorum.yazi = yazi
             yorum.save()
-
-    yorum_ekle_form = YorumEkleModelForm()
-
-    return render(request, 'pages/detay.html', context={
-        'yazi': yazi,
-        'yorumlar': yorumlar,
-        'yorum_ekle_form': yorum_ekle_form
-    })
+            messages.success(request, 'Yorum basarili bir sekilde eklendi.')
+        return redirect('detay', slug=slug)
