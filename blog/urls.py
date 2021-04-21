b37730140295a26b76986a3b7e50422098083755
cprm <soner@arslanyapi.com.tr>
2021-04-21 14:09:55 +0300

    update views duzenlendi

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from blog.views import iletisim, anasayfa, KategoriListView, yazilarim, DetayView, yazi_ekle, yazi_guncelle, yazi_sil, yorum_sil
+from blog.views import iletisim, anasayfa, KategoriListView, yazilarim, DetayView, YaziEkleCreateView, YaziGuncelleUpdateView, \
+    YaziSilDeleteView, yorum_sil
 from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
@@ -10,9 +11,9 @@ urlpatterns = [
     path('kategori/<slug:kategoriSlug>', KategoriListView.as_view(), name='kategori'),
     path('yazilarim', yazilarim, name='yazilarim'),
     path('detay/<slug:slug>', DetayView.as_view(), name='detay'),
-    path('yazi-sil/<slug:slug>', yazi_sil, name='yazi-sil'),
-    path('yazi-ekle', yazi_ekle, name='yazi-ekle'),
+    path('yazi-sil/<slug:slug>', YaziSilDeleteView.as_view(), name='yazi-sil'),
+    path('yazi-ekle', YaziEkleCreateView.as_view(), name='yazi-ekle'),
     path('yorum-sil/<int:id>', yorum_sil, name='yorum-sil'),
-    path('yazi-guncelle/<slug:slug>', yazi_guncelle, name='yazi-guncelle'),
+    path('yazi-guncelle/<slug:slug>', YaziGuncelleUpdateView.as_view(), name='yazi-guncelle'),
 
 ]
--- blog/views/yazi_sil.py
+++ blog/views/yazi_sil.py
@@ -1,9 +1,14 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from blog.models import YazilarModel
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
 
 
-@login_required(login_url='/')
-def yazi_sil(request, slug):
-    get_object_or_404(YazilarModel, slug=slug, yazar=request.user).delete()
-    return redirect('yazilarim')
+class YaziSilDeleteView(DeleteView):
+    template_name = 'pages/yazi_sil_onay.html'
+    success_url = reverse_lazy('yazilarim')
+
+    def get_queryset(self):
+        yazi = YazilarModel.objects.filter(slug=self.kwargs['slug'], yazar=self.request.user)
+        return yazi
